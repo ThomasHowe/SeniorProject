849ab73ab791c227be331108724d0d97229af0c5
ThomasHowe <jimmydean4breakfast@gmail.com>
2016-09-15 15:09:45 -0400

    Starting Code for Communicating with GPS

--- serial.c
+++ serial.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void initialize_pins(void); //Initialize Serial Pins to RX/TX modes
+char *Receive_Coordinates(void) //Receives the string from GPS
+
+
+int main(){
+  
+  
+}
+
+void initialize_pins(void){
+  
+}
