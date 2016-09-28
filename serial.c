e93bea6904a5218f48fb1457bafaad01f7f5c8e1
ThomasHowe <jimmydean4breakfast@gmail.com>
2016-09-28 16:50:33 -0400

    Added code to New function for parsing GPGGA data in C language

--- serial.c
+++ serial.c
@@ -6,6 +6,7 @@ void initialize_pins(void); //Initialize Serial Pins to RX/TX modes
 char *Receive_Coordinates(void); //Receives the string from GPS
 int GPS_Status(char *long, char *lat, char *alt);
 void transmit_coord(char *message);
+void parse(void);
 
 int main(){
   int i;
@@ -18,7 +19,7 @@ int main(){
       //Save Last measured Coord to be sent periodically
   }
   packet = Receive_Coordinates();
-  //Write Code to Parse GPS Packet
+  parse();
   transmit_coord(transmission);
   
 }
@@ -34,3 +35,23 @@ int GPS_Status(char *longi, char *lat, char *alt){
    //Check if GPS is on or off already
    //Altitude Checking
    //Check to see if coord are same as last time
+}
+
+void parse(){
+   int i = 0;
+   int n = 1;
+   if(char[0] == 'G' && char[1] == 'P' && char[2] == 'G' && char[3] == 'G' && char[4] == 'A'){  //Checks to make sure its GPGGA msg
+      for(i=0; i<Sizeof(char);i++)                                                              //Traverses entire message
+          if (char[i] == ',') count++;                                                          //counts number of commas they are separators
+          if(count == 2){                                                                       //if comma count is 2, get lattitude
+             while(char[i+n] != ','){                                                           //go until hits next comma
+                lat[n] = char[i+n];
+                n++;
+                if(char[i+n] == ',') lat[n+1] = char[i+n+1];                                    //Adds direction of coordinates
+             }
+             i = i+n;                                                                           //Move to i+n in message and
+             count = 3;                                                                         //increment comma count
+          }
+   }
+}
+}
