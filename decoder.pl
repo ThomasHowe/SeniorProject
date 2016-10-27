2aed9de25dd817d035c17f9f6c52fc7d1154844e
Alan Kwok <alanwek@gmail.com>
2016-10-27 18:47:39 -0400

    Changes to prove a transmission is occuring

--- decoder.pl
+++ decoder.pl
@@ -18,16 +18,26 @@ Device::BCM2835::gpio_fsel(&Device::BCM2835::RPI_GPIO_P1_11, &Device::BCM2835::B
 #GPIO 17
 Device::BCM2835::gpio_fsel(&Device::BCM2835::RPI_GPIO_P1_17, &Device::BCM2835::BCM2835_GPIO_FSEL_INP);
 
-#Average speed of five dots
-@speed = (0, 0, 0, 0, 0);
-for($i = 1; $i < 5; $i++) {
-    while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 0);
-    $sttime = DateTime->now();
-    while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 1);
-    $endtime = DateTime->now();
-    $elapsed = $endtime - $sttime;
-    $elapsed->in_units('miliseconds');
-    @speed($i) = $elapsed;
+#If a full 1 second beep is sounded, it means get the average speed after
+while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 0);
+$sttime = DateTime->now();
+while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 1);
+$endtime = DateTime->now();
+$elapsed = $endtime - $sttime;
+$elapsed->in_units('miliseconds');
+
+if($elapsed > 1000) {
+    #Average speed of five dots
+    @speed = (0, 0, 0, 0, 0);
+    for($i = 1; $i < 5; $i++) {
+        while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 0);
+        $sttime = DateTime->now();
+        while(Device::BCM2835::gpio_read(&Device::BCM2835::RPIO_GPIO_P1_17, 1);
+        $endtime = DateTime->now();
+        $elapsed = $endtime - $sttime;
+        $elapsed->in_units('miliseconds');
+        @speed($i) = $elapsed;
+    }
 }
 
 my arg1 = mean(@speed);
