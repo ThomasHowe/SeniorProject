4012ee38cbb992ddb0cd3c2cc6e16f996573e41b
ThomasHowe <jimmydean4breakfast@gmail.com>
2016-10-28 15:34:10 -0400

    Added Tons of comments and removed some unnecessary code

--- attempt.c
+++ attempt.c
@@ -6,21 +6,21 @@
 #define ONEHUNDREDMS 0x33333						//Delayms value for delaying onehundred ms
 #define SEVENHMS 818500									//To use as Delay between Words
 
-struct let{
+struct let{															//struct to hold morse code dots and dashes
 	int size;
 	int letval;
 };
 
-void initialize_USART(void);
-void timerinitialize(void);
-void timer(void);
-void transmitstring(char **message, int *sizes);
-void dot(void);
-void dash(void);
-void Toggle_gpio(void);
-void delayms(int time);
-void SleepUsart(void);
-struct let chooseemit(char a, struct let letter);
+void initialize_USART(void);												//Initializes USART pins to talk to GPS
+char Receive_Byte(void);														//Receives one byte from RDR register in USART1								
+void blinkledtest(void);														//Test code for waking up processor from WFI
+void transmitstring(char **message, int *sizes);		//Transmits GPS code, sends words with sizes of words stored in sizes array
+void dot(void);																			//Transmits a Dot and turns off GPIO after
+void dash(void);																		//Transmits a Dash and turns off GPIO after
+void Toggle_gpio(void);															//Turns off the GPIO currently used to transmit
+void delayms(int time);															//Delay function, will put microprocessor to sleep, one hundred ms has been defined
+void SleepUsart(void);															//Currently not implemented, will put GPS to sleep mode to save power
+struct let chooseemit(char a, struct let letter);		//Switch case function to determine morse code from letter
 
 int main(void) {
 	int i=1;
@@ -31,7 +31,7 @@ int main(void) {
 	for(i=0;i<5;i++){
 		message[i] = (char *)malloc(10 * sizeof(char *));
 	}
-	
+																										//Test Message to be sent via morse code 
 	message[0][0] = 'h';
 	message[0][1] = 'e';
 	message[0][2] = 'l';
@@ -55,120 +55,46 @@ int main(void) {
 	size[1] = 5;
 	size[2] = 3;
 	size[3] = 3;
-	size[4] = 3;
-	//SystemCoreClockConfigure();
-	//timerinitialize();
-	RCC->IOPENR  |=  ((1UL << 0));
-	GPIOA->MODER   &= ~((3UL << 2*4));
-  GPIOA->MODER   |=  ((1UL << 2*4));
-  GPIOA->OTYPER  &= ~((1UL <<   4));
-  GPIOA->OSPEEDR &= ~((3UL << 2*4));
+	size[4] = 4;
+	/*------------------------------------------------------------------*/
+	/*----------------------Initialize GPIO Pins------------------------*/
+	RCC->IOPENR  |=  ((1UL << 0));							//Right now using GPIO4 as output
+	GPIOA->MODER   &= ~((3UL << 2*4));				
+  GPIOA->MODER   |=  ((1UL << 2*4));					//Set GPIO4 as output mode
+  GPIOA->OTYPER  &= ~((1UL <<   4));					//sets output type
+  GPIOA->OSPEEDR &= ~((3UL << 2*4));					//Sets speed
   GPIOA->OSPEEDR |=  ((1UL << 2*4));
-  GPIOA->PUPDR   &= ~((3UL << 2*4));
-	//GPIOA->ODR |= ((1UL << 4));
-	//while(1);
-	/*for(i=0;i<10000;i++){
-	}
-	GPIOA->ODR &= ~((1UL << 4));
-	//while(1){
-		GPIOA->ODR |= ((1UL << 4));
-		timerinitialize();
-		timer();
-		GPIOA->ODR &= ~((1UL << 4));
-		timer();
-	//}
-	while(1);*/
-	/*for(i=0;i<5;i++){
-		free(message[i]);
-	}
-	free(message);*/
-		//i++;
-		//delayms(0xFFFFE);
-		//GPIOA->ODR |= ((1UL << 4));
-		//__nop();
-		
-		//while(1);
-		//__nop();
-		//GPIOA->ODR &= ((1UL << 4));
-		//while(1);
-	
+  GPIOA->PUPDR   &= ~((3UL << 2*4));					//no pull up pull down
+	/*------------------------------------------------------------------*/
+	blinkledtest();															//Blinks LED half a second on and off to test
+	initialize_USART();													//initialize the pins for USART comm
+	message[4][3] = Receive_Byte();							//Receive one byte from GPS for now
 	transmitstring(message, size);
 	for(i=0;i<5;i++){
 		free(message[i]);
 	}
 	free(message);
-	/*while(1){
-		GPIOA->ODR |= ((1UL << 4));
-		delayms(5 * ONEHUNDREDMS);
-		delayms(5 * ONEHUNDREDMS);
-	}*/
 	while(1);
 }
 
 
-void timerinitialize(void){
-	/*NVIC_EnableIRQ(TIM2_IRQn);
-	NVIC_SetPriority(TIM2_IRQn, 1);
-	RCC->APB1ENR &= ~(0x80000000);
-	RCC->APB1ENR |= 0x80000000;
-	RCC->APB1SMENR |= 0x80000000;
-	RCC->CCIPR &= ~(0xC0000);
-	RCC->CCIPR |=  (0xC0000);
-	//LPTIM1->CFGR |= 0xC00;
-	//LPTIM1->CFGR |= 
-	LPTIM1->IER |= 0x2;
-	LPTIM1->ARR &= ~(0xFFFF);
-	LPTIM1->ARR |= 0x00FF;
-	LPTIM1->CMP |= 0xFFFF;
-	LPTIM1->CR |= 0x1;
-	LPTIM1->CR|=0x2;*/
-	/*RCC->APB1ENR |= 0x1;
-	RCC->APB1SMENR |= 0x1;
-	//TIM2->CR1 |= 0x8;
-	//TIM2->CR1 |= 0x10;
-	TIM2->PSC |= 0xFFFF;
-	NVIC_EnableIRQ(SysTick_IRQn);
-	NVIC_SetPriority(SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);
-	TIM2->DIER |= 0x0002;
-	TIM2->ARR = 0xCFF0;
-	TIM2->CCR1 |= 0x100;
-	TIM2->EGR |= TIM_EGR_CC1G;*/
-	//TIM2->CR1 |= TIM_CR1_CEN;
-	SysTick->CTRL = 0;
-	SysTick->LOAD = 0xFFFFE;
-	NVIC_SetPriority(SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);
-	SysTick->VAL = 0;
-	SysTick->CTRL |= 0x4;
-	SysTick->CTRL |= 0x1;
-	SysTick->CTRL |= 0x2;
-}	
 
-void timer(void){
-	//int i;
-	//TIM2->CR1 |= TIM_CR1_CEN;
-	//for(i=0;i<10000;i++);
-	//LPTIM1->CR |= 0x1;
-	//LPTIM1->CR|=0x2;
-	//TIM2->SR &= ~(0x1E5F);
-	//TIM2->SR &= ~(0x1E5F);
+
+void blinkledtest(void){
+	
 	GPIOA->ODR |= ((1UL << 4));
 	while(1){
-		delayms(1048574);
-
+		GPIOA->ODR |= ((1UL << 4));
+		delayms(5 * ONEHUNDREDMS);
+		delayms(5 * ONEHUNDREDMS);
 	}
-	//__wfi();
-	//TIM2->SR &= ~(0x0001);
 }
-void SysTick_Handler(void){
-	//GPIOA->ODR |= ((1UL << 4));
-	Toggle_gpio();
-	
-	SysTick->CTRL = 0;
-	/*int i = 0;
-	i++;*/
+void SysTick_Handler(void){									//Goes here when Systick match interrupt occurs
+	Toggle_gpio();														//Turns off GPIO pin
+	SysTick->CTRL = 0;												//Turn off the systick timer so no more interrupts occur
 }
 
-struct let chooseemit(char a, struct let letter){
+struct let chooseemit(char a, struct let letter){		//Crazy switch case function to set up morse code
 	switch (a) {
 		case 'a': {
 			letter.letval = 2;
@@ -360,6 +286,16 @@ struct let chooseemit(char a, struct let letter){
 			letter.size = 6;
 			return letter;
 		}
+		case ',': {
+			letter.letval = 33;
+			letter.size = 6;
+			return letter;
+		}
+		case 'G': {
+			letter.letval = 33;
+			letter.size = 6;
+			return letter;
+		}
 		default : {
 			//printf(“Only lower case letters no spaces”);
 			exit(2);			//error checking
@@ -367,25 +303,26 @@ struct let chooseemit(char a, struct let letter){
 }
 }
 
-void initialize_USART(void){
-	GPIOA->MODER &= ~((0x00200000));
-	GPIOA->MODER |=  ((0x00200000));
-	GPIOA->AFR[1] &= ~((0x00000F00));
+void initialize_USART(void){										//Set up pins for USART1
+	GPIOA->MODER = (GPIOA->MODER & ~(GPIO_MODER_MODE10)) | (GPIO_MODER_MODE10_1);
+	GPIOA->AFR[1] = (GPIOA->AFR[1] & ~(0x00000FF0)) | (4 << (1 * 4)) | (4<<(2*4));
+	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 	USART1->BRR = 160000 / 96; /* (1) */
-	USART1->CR1 = USART_CR1_RXNEIE | USART_CR1_RE | USART_CR1_UE;
+	USART1->CR1 = USART_CR1_RE | USART_CR1_UE;
 }
 
 char Receive_Byte(void){
 	char x;
+	while((USART1->ISR & USART_ISR_RXNE) != USART_ISR_RXNE);
 	if((USART1->ISR & USART_ISR_RXNE) == USART_ISR_RXNE){
 		x = (uint8_t)(USART1->RDR); /* Receive data, clear flag */
 	}
 	return x;
 }
 
-void SleepUsart(void){					//Page 19 on Command Sheet for more info
+/*void SleepUsart(void){					//Page 19 on Command Sheet for more info
 	
-}
+}*/
 
 
 void transmitstring(char **message, int *sizes){
@@ -416,23 +353,18 @@ void dot(void) {
 }
 
 void delayms(int time){
-	SysTick->CTRL = 0;
-	SysTick->LOAD = time;
-	NVIC_SetPriority(SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);
-	SysTick->VAL = 0;
-	SysTick->CTRL |= 0x4;
+	SysTick->CTRL = 0;							//Clear systick control register to make sure it is off
+	SysTick->LOAD = time;						//SET systick load to set time of systick timer
+	NVIC_SetPriority(SysTick_IRQn, (1<<__NVIC_PRIO_BITS) - 1);	//Turn on the interrupt
+	SysTick->VAL = 0;								//clearerr timer
+	SysTick->CTRL |= 0x4;						
 	SysTick->CTRL |= 0x1;
-	SysTick->CTRL |= 0x2;
-	__wfi();
+	SysTick->CTRL |= 0x2;						//Turn on systick timer
+	__wfi();												//Set processor into sleep mode and will wake up when interupt occurs.
 }
 
 void Toggle_gpio(void){
-	/*if((GPIOA->ODR & GPIO_ODR_OD4) == GPIO_ODR_OD4){
-		GPIOA->ODR &= ~((1UL << 4));
-	}
-	else GPIOA->ODR |= ((1UL << 4));*/
 	GPIOA->ODR &= ~((1UL << 4));
 	;
 }
-
 
