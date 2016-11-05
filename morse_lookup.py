d1cc6c8db0987ce51a0b9a414110451dac8fc21f
Alan Kwok <alanwek@gmail.com>
2016-11-05 15:07:56 -0400

    modified a py morse lookup script

---
+++ morse_lookup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import sys
+
+#modified from https://github.com/raspberrypilearning/morse-code-virtual-radio/blob/master/code/morse_lookup.py
+
+morse_code_lookup = {
+    ".-":    "A",
+    "-...":    "B",
+    "-.-.":    "C",
+    "-..":    "D",
+    ".":    "E",
+    "..-.":    "F",
+    "--.":    "G",
+    "....":    "H",
+    "..":    "I",
+    ".---":    "J",
+    "-.-":    "K",
+    ".-..":    "L",
+    "--":    "M",
+    "-.":    "N",
+    "---":    "O",
+    ".--.":    "P",
+    "--.-":    "Q",
+    ".-.":    "R",
+    "...":    "S",
+    "-":    "T",
+    "..-":    "U",
+    "...-":    "V",
+    ".--":    "W",
+    "-..-":    "X",
+    "-.--":    "Y",
+    "--..":    "Z",
+    ".----":    "1",
+    "..---":    "2",
+    "...--":    "3",
+    "....-":    "4",
+    ".....":    "5",
+    "-....":    "6",
+    "--...":    "7",
+    "---..":    "8",
+    "----.":    "9",
+    "-----":    "0",
+    ".-.-.-":   ".",
+    "--..--":   ",",
+    ".-.-.":    "+",
+    "-....-":   "-"
+    
+}
+
+def try_decode(bit_string):
+    if bit_string in morse_code_lookup.keys():
+        sys.stdout.write(morse_code_lookup[bit_string])
+        sys.stdout.flush()
