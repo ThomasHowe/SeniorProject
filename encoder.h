635cc6e210261a994a1523fe9d86217112ac6040
Alan Kwok <alanwek@gmail.com>
2016-12-12 16:52:55 -0500

    Header file changes

--- encoder.h
+++ encoder.h
@@ -12,18 +12,26 @@
 #ifndef ENCODER.H
 #define ENCODER.H
 
+//Struct used to determine what character something is and how it should be transmitted in Morse
 struct let{
 	int size;
 	int letval;
 };
 
+//Initilaize timer
 void timerinitialize(void);
 void timer(void);
+//Transmit a string in Morse code
 void transmitstring(char **message, int *sizes);
+//A dot in Morse code
 void dot(void);
+//A dash in Morse code
 void dash(void);
+//Turn on and off
 void Toggle_gpio(void);
+//Control time delay to control length of dots and dashes for different speeds
 void delayms(int time);
+//Struct for choosing characters
 struct let chooseemit(char a, struct let letter);
 
 #endif
